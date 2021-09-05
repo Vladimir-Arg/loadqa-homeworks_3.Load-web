acb617be7ecdc06d7439fedca97f71b3ae7b79cb
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2021-09-05 22:18:23 +0300

    Fix docker-compose. Add dockerfile

--- cinema/www/config/autoloader.php
+++ cinema/www/config/autoloader.php
@@ -3,8 +3,8 @@
     const DB_NAME = "database";
     const BD_USER_NAME = "admin";
     const BD_PASSWORD = "test";
-    const BD_HOST = "0.0.0.0";
-    const DB_PORT = 8889;
+    const BD_HOST = "db";
+    const DB_PORT = 3306;
 
     spl_autoload_register(
         function($class_name)
